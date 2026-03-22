efb25688a0bdcd89cc9a884d48d0e84935ab8b8c
Justin Walker <justinwalker.contact@gmail.com>
2026-03-22 11:28:21 -0700

    refactor: minor refinements

--- bidding-engine/src/test/java/com/walker/bidding/TestBiddingEngineApplication.java
+++ bidding-engine/src/test/java/com/walker/bidding/TestBiddingEngineApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 
 public class TestBiddingEngineApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.from(BiddingEngineApplication::main).with(TestcontainersConfiguration.class).run(args);
 	}
 
